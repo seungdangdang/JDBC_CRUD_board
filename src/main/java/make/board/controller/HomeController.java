757aaa192cc09daccdbae86d0fd1655eb06f9ad1
yunseung <yseungun.21@gmail.com>
2024-01-07 23:08:11 +0900

    feat: home으로 돌아가는 메서드 추가

--- src/main/java/make/board/controller/HomeController.java
+++ src/main/java/make/board/controller/HomeController.java
@@ -5,10 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ProjectController {
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 
     @GetMapping("update")
-    public String hello(Model model) {
+    public String update(Model model) {
         model.addAttribute("data", "게시글 등록하기");
         return "update";
     }
