b934e50af56f96d9a07053cbf70dc4288a22a062
yunseung <yseungun.21@gmail.com>
2024-01-08 23:12:27 +0900

    feat: view 컨트롤러 작성

--- src/main/java/make/board/controller/HomeController.java
+++ src/main/java/make/board/controller/HomeController.java
@@ -1,11 +1,20 @@
 package make.board.controller;
 
+import java.util.List;
+import make.board.domain.Post;
+import make.board.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    private PostService postService;
+
+    public HomeController(PostService postService) {
+        this.postService = postService;
+    }
 
     @GetMapping("/")
     public String home() {
@@ -16,5 +25,12 @@ public class HomeController {
     public String update(Model model) {
         model.addAttribute("data", "게시글 등록하기");
         return "update";
+    }
+
+    @GetMapping("view")
+    public String view(Model model) {
+        List<Post> posts = postService.findPosts();
+        model.addAttribute("posts", posts);
+        return "view";
     }
 }
