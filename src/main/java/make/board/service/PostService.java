74c43ff7d0f77776e7c3c974337c7e3417de26d5
yunseung <yseungun.21@gmail.com>
2024-01-08 20:31:06 +0900

    feat: service 관련 메서드 생성

---
+++ src/main/java/make/board/service/PostService.java
@@ -0,0 +1,32 @@
+package make.board.service;
+
+import java.util.List;
+import java.util.Optional;
+import make.board.domain.Post;
+import make.board.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Long join(Post post) {
+        postRepository.save(post);
+        return post.getId();
+    }
+
+    public List<Post> findPosts() {
+        return postRepository.findAll();
+    }
+
+    public Optional<Post> findOne(Long postId) {
+        return postRepository.findById(postId);
+    }
+}
