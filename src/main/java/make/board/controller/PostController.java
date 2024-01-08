5b8e79450448a6333a6e0a204dec1fa1724ea1d5
yunseung <yseungun.21@gmail.com>
2024-01-08 21:43:45 +0900

    feat: post생성 컨트롤러 추가

--- src/main/java/make/board/controller/PostController.java
+++ src/main/java/make/board/controller/PostController.java
@@ -1,26 +1,37 @@
 package make.board.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import make.board.domain.Post;
+import make.board.service.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class PostController {
 
-    @PostMapping("/post/new")
-//    @ResponseStatus(HttpStatus.OK)
-    public String handlePostRequest(@RequestParam(name = "inputName") String name,
-                                    @RequestParam(name = "inputTitle") String title,
-                                    @RequestParam(name = "inputContent") String content) {
-        //로그용
-        System.out.println(name + title + content);
+    private PostService postService;
 
-//        return ResponseEntity.badRequest().build();
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping("/post/new")
+    public String create(@ModelAttribute Post post) {
+        // 로그용
+        System.out.println(post.getInputName() + post.getInputTitle() + post.getInputContent());
+
+        postService.join(post);
 
         return "redirect:/";
     }
+//    @ResponseStatus(HttpStatus.OK)
+//    public String handlePostRequest(@RequestParam(name = "inputName") String name,
+//                                    @RequestParam(name = "inputTitle") String title,
+//                                    @RequestParam(name = "inputContent") String content) {
+//        로그용
+//        System.out.println(name + title + content);
+
+////        return ResponseEntity.badRequest().build();
+//        return "redirect:/";
+//    }
 }
