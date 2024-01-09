649d144d2f06dd11a5ef1d2a37898ffeed817be5
yunseung <yseungun.21@gmail.com>
2024-01-09 23:12:16 +0900

    feat: detail 컨트롤러 추가

--- src/main/java/make/board/controller/PostController.java
+++ src/main/java/make/board/controller/PostController.java
@@ -1,10 +1,14 @@
 package make.board.controller;
 
+import java.util.Optional;
 import make.board.domain.Post;
 import make.board.service.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PostController {
@@ -34,4 +38,12 @@ public class PostController {
 ////        return ResponseEntity.badRequest().build();
 //        return "redirect:/";
 //    }
+
+    @GetMapping("/detail")
+    public String showDetailPage(@RequestParam(name = "postId") Long postId, Model model) {
+        Optional<Post> optionalPost = postService.findPostById(postId);
+        optionalPost.ifPresent(post -> model.addAttribute("post", post));
+
+        return "detail";
+    }
 }
