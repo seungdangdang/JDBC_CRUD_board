7c47d41e1d49359251d9bee989e66b1bf28e73c4
yunseung <yseungun.21@gmail.com>
2024-01-11 21:51:01 +0900

    feat: 공백이 입력되었는지 검사하는 메서드 추가

--- src/main/java/make/board/service/PostService.java
+++ src/main/java/make/board/service/PostService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import make.board.domain.Post;
 import make.board.repository.PostRepository;
+import org.springframework.beans.NullValueInNestedPathException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,7 @@ public class PostService {
     }
 
     public Long join(Post post) {
+        validateJoin(post);
         postRepository.save(post);
         return post.getId();
     }
@@ -32,5 +34,11 @@ public class PostService {
 
     public void delete(Long postId) {
         postRepository.delete(postId);
+    }
+
+    private void validateJoin(Post post) {
+        if (post.getInputName().isEmpty() || post.getInputTitle().isEmpty() || post.getInputContent().isEmpty()) {
+            throw new NullPointerException("모두 입력하세요.");
+        }
     }
 }
