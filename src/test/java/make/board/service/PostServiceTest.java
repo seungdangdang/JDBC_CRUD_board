a9beb09b3186056d3686cd7c0223a1efca6434b7
yunseung <yseungun.21@gmail.com>
2024-01-12 23:30:45 +0900

    test: delete 테스트 코드 작성

--- src/test/java/make/board/service/PostServiceTest.java
+++ src/test/java/make/board/service/PostServiceTest.java
@@ -1,6 +1,7 @@
 package make.board.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Optional;
 import make.board.domain.Post;
 import make.board.repository.PostRepository;
@@ -40,14 +41,33 @@ class PostServiceTest {
     }
 
     @Test
-    void findPosts() {
-    }
-
-    @Test
     void findPostById() {
     }
 
     @Test
     void delete() {
+        Post post = new Post();
+        post.setInputName("홍길동");
+        post.setInputTitle("홍길동전");
+        post.setInputContent("옛날옛적에");
+
+        Long id = postService.join(post);
+
+        Optional<Post> findPost = postService.findPostById(id);
+
+        // Optional에서 값 가져오기
+        Post actualPost = findPost.orElseThrow(() -> new AssertionError("게시물을 찾을 수 없습니다."));
+
+        // 테스트 검증
+        assertThat(actualPost.getInputName()).isEqualTo("홍길동");
+        assertThat(actualPost.getInputTitle()).isEqualTo("홍길동전");
+        assertThat(actualPost.getInputContent()).isNotEqualTo("옛날옛적에말입니다");
+
+        postService.delete(id);
+
+        Optional<Post> secondPost = postService.findPostById(id);
+
+        // Optional에서 값 가져오기
+        assertThat(secondPost).isEmpty();
     }
 }
