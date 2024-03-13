4270cc79d8c7fa5753fcf9603a7c30ae4809af77
yunseung <yseungun.21@gmail.com>
2024-03-13 15:59:41 +0900

    chore: html 이름 수정

--- src/main/java/make/board/controller/HomeController.java
+++ src/main/java/make/board/controller/HomeController.java
@@ -21,10 +21,10 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("update")
-    public String update(Model model) {
+    @GetMapping("create")
+    public String create(Model model) {
         model.addAttribute("data", "게시글 등록하기");
-        return "update";
+        return "create";
     }
 
     @GetMapping("view")
