52306e5924c01698c9c0392d5aaf0ac7fea6145c
yunseung <yseungun.21@gmail.com>
2024-03-17 19:32:03 +0900

    feat: signup controller create

---
+++ src/main/java/make/board/user/controller/UserController.java
@@ -0,0 +1,42 @@
+package make.board.user.controller;
+
+import make.board.user.domain.SiteUser;
+import make.board.user.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("data", "회원가입 하기");
+        return "signup_form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute SiteUser siteUser, RedirectAttributes redirectAttributes) {
+        try {
+            // 로그용
+            System.out.println(siteUser.toString());
+
+            userService.create(siteUser);
+            return "redirect:/";
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+            return "redirect:/signup";
+        }
+    }
+}
