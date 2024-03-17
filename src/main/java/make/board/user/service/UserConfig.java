53ddbd26fa8660a46331cd040d3076267820ba90
yunseung <yseungun.21@gmail.com>
2024-03-17 16:46:04 +0900

    feat: UserConfig create

---
+++ src/main/java/make/board/user/service/UserConfig.java
@@ -0,0 +1,28 @@
+package make.board.user.service;
+
+
+import javax.sql.DataSource;
+import make.board.user.repository.JdbcUserRepository;
+import make.board.user.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserConfig {
+
+    private final DataSource dataSource;
+
+    public UserConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public UserService userService() {
+        return new UserService(userRepository());
+    }
+
+    @Bean
+    public UserRepository userRepository() {
+        return new JdbcUserRepository(dataSource);
+    }
+}
