16a51b0cd10f4be7705cb62045fd096614c4c65d
yunseung <yseungun.21@gmail.com>
2024-03-17 19:32:48 +0900

    feat: create Repository Function create

--- src/main/java/make/board/user/repository/JdbcUserRepository.java
+++ src/main/java/make/board/user/repository/JdbcUserRepository.java
@@ -1,7 +1,13 @@
 package make.board.user.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.sql.DataSource;
 import make.board.user.domain.SiteUser;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 public class JdbcUserRepository implements UserRepository {
 
@@ -13,6 +19,67 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public SiteUser create(SiteUser siteUser) {
-        return null;
+        String sql = "INSERT INTO siteUser(username, password, email) VALUES (?, ?, ?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstmt.setString(1, siteUser.getUsername());
+            pstmt.setString(2, siteUser.getPassword());
+            pstmt.setString(3, siteUser.getEmail());
+
+            pstmt.executeUpdate();
+
+            rs = pstmt.getGeneratedKeys();
+
+            if (rs.next()) {
+                siteUser.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+
+            return siteUser;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
     }
 }
