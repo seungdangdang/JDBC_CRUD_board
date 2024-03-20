3dd812ab54724fa62f4b572142b7268c8d214842
yunseung <yseungun.21@gmail.com>
2024-03-20 17:17:10 +0900

    feat: "post/new" CSRF 보호 무시

--- src/main/java/make/board/user/config/SecurityConfig.java
+++ src/main/java/make/board/user/config/SecurityConfig.java
@@ -21,7 +21,8 @@ public class SecurityConfig  {
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
                 .csrf((csrf) -> csrf
-                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/post/new")))
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
